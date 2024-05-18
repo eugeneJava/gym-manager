8333b2f969525afd7b7a1dcbe8039896cd9f9af4
Eugene Turovskiy <liquidnost@yahoo.com>
2024-05-18 22:02:33 +0300

    ddd

---
+++ release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd ~/IdeaProjects/gym-manager/backend/target
+cp backend-1.0.jar ~/release/
+
+FILE="~/release/backend-1.0.jar"
+
+# Check if the file exists
+if [ -f "$FILE" ]; then
+    echo "The file $FILE copied successfully."
+else
+    echo "The file $FILE does not exist."
+fi
