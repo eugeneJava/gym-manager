848f5ac08618c78631ee5f8c84b58cb69c055746
Eugene Turovskiy <liquidnost@yahoo.com>
2026-05-11 20:46:47 +0300

    added the github workflow

--- backend/src/main/java/ua/gym/app/SecurityConfig.java
+++ backend/src/main/java/ua/gym/app/SecurityConfig.java
@@ -6,7 +6,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -36,7 +35,6 @@ import java.util.Arrays;
 import java.util.function.Supplier;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
-import static org.springframework.security.web.util.matcher.RegexRequestMatcher.regexMatcher;
 
 @Configuration
 @EnableWebSecurity
